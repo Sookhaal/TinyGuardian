5b9dbb57ec41e5555922828923ef4cf0fc053f8e
Nelios <sookhaal@gmail.com>
2017-12-02 16:50:10 +0100

    Enemy pathing is now an offset

--- Assets/Sources/Components/Enemy.cs
+++ Assets/Sources/Components/Enemy.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using Data;
 using DG.Tweening;
 using DG.Tweening.Core;
@@ -19,15 +20,14 @@ namespace Components {
 		private EnemyPathData _pathData;
 		private Vector3 _basePosition;
 		private TweenerCore<Vector3, Path, PathOptions> _pathTweener;
+		private Vector3[] _waypoints;
 
 		private void Awake() {
 			_transform = GetComponent<Transform>();
 			_basePosition = _transform.position;
 			HP = EnemyData.StartingHP;
 			Weapon.SelectWeapon(EnemyData.SelectedWeapon);
-			if (_pathData) {
-				_pathTweener = _transform.DOPath(_pathData.Waypoints, _pathData.Duration, _pathData.PathType);
-			}
+			DoThePath();
 		}
 
 		private void Update() {
@@ -39,13 +39,17 @@ namespace Components {
 		}
 
 		public void DoThePath() {
-			if (_pathTweener == null)
-				return;
+			_pathTweener?.Kill();
 
-			_pathTweener.Kill();
 			_transform.position = _basePosition;
+			_waypoints = new Vector3[_pathData.Waypoints.Length];
+
+			for (var i = 0; i < _waypoints.Length; i++) {
+				_waypoints[i] = _basePosition + _pathData.Waypoints[i];
+			}
+
 			if (_pathData) {
-				_pathTweener = _transform.DOPath(_pathData.Waypoints, _pathData.Duration, _pathData.PathType);
+				_pathTweener = _transform.DOPath(_waypoints, _pathData.Duration, _pathData.PathType);
 			}
 		}
 
