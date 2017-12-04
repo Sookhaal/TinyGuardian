d5a9fe307d74ee4c055027c61d04a17794adc87b
Nelios <sookhaal@gmail.com>
2017-12-04 23:27:06 +0100

    Fix type weakness

--- Assets/Sources/Components/PowerupManager.cs
+++ Assets/Sources/Components/PowerupManager.cs
@@ -20,7 +20,9 @@ namespace Components {
 		}
 
 		public void SpawnPowerup() {
-			Instantiate(_powerUps[_powerUpIndex]);
+			var _powerup = Instantiate(_powerUps[_powerUpIndex]);
+			_powerup.transform.position = new Vector3(12f, 0f, 0f);
+			_powerup.GetComponent<Rigidbody2D>().velocity = new Vector2(-1f, 0f);
 			_powerUpIndex++;
 
 			if (_powerUpIndex < _powerUps.Count) {
